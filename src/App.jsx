ef352e525b8cd1a33ca0334384c63f832cb4d17d
Nathaniel <n.geolier@hotmail.fr>
2024-03-21 14:52:15 +0100

    formulaire

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <H1>"Create account"</H1>
+      </header>
+    </>
+  );
+}
+
+export default App;
